9ccdfb7a977681d6a63e439443f2ace549a35d31
Oskar <69815@student.ue.poznan.pl>
2022-04-10 17:50:52 +0200

    sprawdzenie inputow w sprawdzarce

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=True, order=True)
+class Ksiazka:
+    tytul: str
+    autor: str
+    rok: int
+
+@dataclass(frozen=True, order=True)
+class Biblioteka:
+    ksiazka: Ksiazka
+    egzemplarze: int
+
+
 def main():
-    print('hello')
+    liczba_ksiaze = input()
+    print(liczba_ksiaze)
+
+    for _ in range(int(liczba_ksiaze))
+        ksiazka = input()
+        print(ksiazka)
 
 if __name__ == "__main__":
     main()
