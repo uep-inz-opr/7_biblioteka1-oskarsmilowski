76440845e4aaf7430a5c83cebad0ac67e481b0bd
Oskar <69815@student.ue.poznan.pl>
2022-04-10 18:28:06 +0200

    moze zadziala

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from operator import itemgetter
 import ast
 
 @dataclass(frozen=True, order=True)
@@ -9,19 +10,34 @@ class Ksiazka:
 
 @dataclass(frozen=True, order=True)
 class Biblioteka:
-    ksiazka: Ksiazka
-    egzemplarze: int
+    ksiazki: dict()
+
+    def stan_biblioteki(self):
+        stan_lista = list()
+        for (tytul, autor), egzemplarze in self.ksiazki.items():
+            krotka = tytul,autor,egzemplarze
+            stan_lista.append(krotka)
+        return sorted(stan_lista, key=itemgetter(0))
+
 
 
 def main():
     liczba_ksiaze = input()
     print(liczba_ksiaze)
+    biblioteka = Biblioteka({})
 
     for _ in range(int(liczba_ksiaze)):
         dane_ksiazki = eval(input())
         ksiazka = Ksiazka(*dane_ksiazki)
-        print(ksiazka)
-        print(f'typ: {type(ksiazka)}')
+
+        if (ksiazka.tytul, ksiazka.autor) not in biblioteka.ksiazki:
+            biblioteka.ksiazki[(ksiazka.tytul,ksiazka.autor)] = 1
+        else:
+            biblioteka.ksiazki[(ksiazka.tytul,ksiazka.autor)] += 1
+    stan = biblioteka.stan_biblioteki()
+    for krotka in stan:
+        print(krotka)
+        
 
 if __name__ == "__main__":
     main()
